e0a381bea990f49d6d81704b3fe3ffe4c8c780da
Cole Cecil <cole.cecil@gmail.com>
2025-02-06 23:01:59 -0600

    Update Dagger and start working on functions

--- dagger/src/index.ts
+++ dagger/src/index.ts
@@ -1,43 +1,19 @@
-/**
- * A generated module for ColececilGithubIo functions
- *
- * This module has been generated via dagger init and serves as a reference to
- * basic module structure as you get started with Dagger.
- *
- * Two functions have been pre-created. You can modify, delete, or add to them,
- * as needed. They demonstrate usage of arguments and return types using simple
- * echo and grep commands. The functions can be called from the dagger CLI or
- * from one of the SDKs.
- *
- * The first line in this comment block is a short description line and the
- * rest is a long description with more detail on the module's purpose or usage,
- * if appropriate. All modules should have a short description.
- */
-
 import { dag, Container, Directory, object, func } from "@dagger.io/dagger"
 
 @object()
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-class ColececilGithubIo {
-  /**
-   * Returns a container that echoes whatever string argument is provided
-   */
+export class ColececilGithubIo {
   @func()
-  containerEcho(stringArg: string): Container {
-    return dag.container().from("alpine:latest").withExec(["echo", stringArg])
+  getBuildContainer(src: Directory): Container {
+    return dag
+        .container()
+        .from('jekyll/jekyll:3.8')
+        .withMountedDirectory('/srv/jekyll', src)
   }
 
-  /**
-   * Returns lines that match a pattern in the files of the provided Directory
-   */
   @func()
-  async grepDir(directoryArg: Directory, pattern: string): Promise<string> {
-    return dag
-      .container()
-      .from("alpine:latest")
-      .withMountedDirectory("/mnt", directoryArg)
-      .withWorkdir("/mnt")
-      .withExec(["grep", "-R", pattern, "."])
-      .stdout()
+  build(src: Directory): Directory {
+    return this.getBuildContainer(src)
+        .withExec(['jekyll', 'build'])
+        .directory('/srv/jekyll/_site')
   }
 }
