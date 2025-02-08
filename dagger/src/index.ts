0d5deed79659270261aaae0dcf9aa4d84fbda7db
Cole Cecil <cole.cecil@gmail.com>
2025-02-07 18:03:07 -0600

    Add dev mode function to Dagger

--- dagger/src/index.ts
+++ dagger/src/index.ts
@@ -1,13 +1,15 @@
-import { dag, Container, Directory, object, func } from "@dagger.io/dagger"
+import {dag, Container, Directory, object, func, Service} from "@dagger.io/dagger"
 
 @object()
 export class ColececilGithubIo {
   @func()
-  getBuildContainer(src: Directory): Container {
-    return dag
-        .container()
-        .from('jekyll/jekyll:3.8')
-        .withMountedDirectory('/srv/jekyll', src)
+  devMode(src: Directory): Service {
+    return this.getBuildContainer(src)
+        .withExposedPort(4000)
+        .withExposedPort(35729)
+        .asService({
+          args: ['jekyll', 'serve', '--force_polling', '--livereload']
+        })
   }
 
   @func()
@@ -15,5 +17,13 @@ export class ColececilGithubIo {
     return this.getBuildContainer(src)
         .withExec(['jekyll', 'build'])
         .directory('/srv/jekyll/_site')
+  }
+
+  @func()
+  getBuildContainer(src: Directory): Container {
+    return dag
+        .container()
+        .from('jekyll/jekyll:3.8')
+        .withMountedDirectory('/srv/jekyll', src)
   }
 }
